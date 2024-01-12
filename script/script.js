c735c7ee59bffdcdd17ca9b408b47cbc18e653e3
lisagjh <lisagjhoorn@outlook.com>
2024-01-12 12:24:54 +0100

    update animation interaction

--- script/script.js
+++ script/script.js
@@ -6,3 +6,7 @@ console.log(jiraContent)
 jiraBtn.addEventListener('click', function() {
     jiraContent.classList.toggle("show-content")
 })
+
+
+// carousel: https://medium.com/@algopoint.ltd/how-to-create-a-slideshow-carousel-using-html-css-js-7ab0561b39b3
+
