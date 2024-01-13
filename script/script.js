2cbfd589b067bffccefd48426203d0cc2b49af69
lisagjh <lisagjhoorn@outlook.com>
2024-01-13 13:47:57 +0100

    help :(

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 const jiraBtn = document.querySelector(".jira-btn-one")
 const jiraContent = document.querySelector(".jira-content")
 
-console.log(jiraContent)
-
 jiraBtn.addEventListener('click', function() {
     jiraContent.classList.toggle("show-content")
 })
 
+// carousel
 
-// carousel: https://medium.com/@algopoint.ltd/how-to-create-a-slideshow-carousel-using-html-css-js-7ab0561b39b3
 
+// tabs
+//  https://wesbos.com/javascript/06-serious-practice-exercises/tabs
