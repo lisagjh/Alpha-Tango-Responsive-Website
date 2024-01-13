c9208c89289bb15d7f24d11a71901c15f197829e
lisagjh <lisagjhoorn@outlook.com>
2024-01-13 17:19:19 +0100

    help :'(

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
-const jiraBtn = document.querySelector(".jira-btn-one")
-const jiraContent = document.querySelector(".jira-content")
-
-jiraBtn.addEventListener('click', function() {
-    jiraContent.classList.toggle("show-content")
-})
-
-// carousel
-
-
 // tabs
 //  https://wesbos.com/javascript/06-serious-practice-exercises/tabs
+
+
+const tabJira = document.querySelector(".tab-jira")
+const tabConf = document.querySelector(".tab-conf")
+
+const carouselJira = document.querySelector("#jira-tab-content")
+const carouselConf = document.querySelector("#conf-tab-content")
+
+tabJira.addEventListener('click', function(){
+    carouselJira.classList.toggle(".show-app-content")
+})
+
+console.log(tabJira)
