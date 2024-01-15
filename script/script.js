5b46a3c68f187e34a9a100ca08554bc6cbc35cc2
lisagjh <lisagjhoorn@outlook.com>
2024-01-15 15:11:22 +0100

    EINDELIJK

--- script/script.js
+++ script/script.js
@@ -1,18 +1,43 @@
 // tabs
-//  https://wesbos.com/javascript/06-serious-practice-exercises/tabs
 
-const tabJira = document.querySelector(".tab-jira");
-const tabConf = document.querySelector(".tab-conf");
+// const tabJira = document.querySelector(".tab-jira");
+// const tabConf = document.querySelector(".tab-conf");
 
-const contentJira = document.querySelector("#jira-tab-content");
-const contentConf = document.querySelector("#conf-tab-content");
+// const contentJira = document.querySelector("#jira-tab-content");
+// const contentConf = document.querySelector("#conf-tab-content");
 
-tabJira.addEventListener('click', function(){
-    contentJira.classList.toggle("show");
-});
+// tabJira.addEventListener('click', function(){
+//     contentJira.classList.toggle("show");
+// });
 
-tabConf.addEventListener('click', function(){
-    contentConf.classList.toggle("show");
-})
+// tabConf.addEventListener('click', function(){
+//     contentConf.classList.toggle("show");
+// })
 
-console.log(contentConf)
+//  tabs v2 
+
+document.addEventListener("DOMContentLoaded", function () {
+    var JiraBtn = document.querySelector(".tab-jira");
+    var ConfBtn = document.querySelector(".tab-conf");
+    var JiraContent = document.querySelector("#jira-tab-content");
+    var ConfContent = document.querySelector("#conf-tab-content");
+  
+    ConfBtn.addEventListener("click", function () {
+      showPage(ConfContent, JiraContent);
+    });
+  
+    JiraBtn.addEventListener("click", function () {
+      showPage(JiraContent, ConfContent);
+    });
+  
+    // Initial setup: Show page 1
+    showPage(ConfContent, JiraContent);
+  });
+  
+  function showPage(show, hide) {
+    // Show the selected page
+    show.classList.add("show");
+  
+    // Hide the other page
+    hide.classList.remove("show");
+  }
