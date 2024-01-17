cd8babfaa44095aa9ce8eb70db65e49ca5a63422
lisagjh <lisagjhoorn@outlook.com>
2024-01-17 11:03:41 +0100

    dfghjk

--- script/script.js
+++ script/script.js
@@ -1,42 +1,29 @@
-// tabs
-
-// const tabJira = document.querySelector(".tab-jira");
-// const tabConf = document.querySelector(".tab-conf");
-
-// const contentJira = document.querySelector("#jira-tab-content");
-// const contentConf = document.querySelector("#conf-tab-content");
-
-// tabJira.addEventListener('click', function(){
-//     contentJira.classList.toggle("show");
-// });
-
-// tabConf.addEventListener('click', function(){
-//     contentConf.classList.toggle("show");
-// })
-
 //  tabs v2
 
-document.addEventListener("DOMContentLoaded", function () {
   var JiraBtn = document.querySelector(".tab-jira");
   var ConfBtn = document.querySelector(".tab-conf");
   var JiraContent = document.querySelector("#jira-tab-content");
   var ConfContent = document.querySelector("#conf-tab-content");
+  var header = document.querySelector(".header-wrapper");
 
   ConfBtn.addEventListener("click", function () {
     showPage(ConfContent, JiraContent);
-    ConfBtn.classList.add('active-conf-btn');
+    ConfBtn.classList.remove('tab-conf-passive');
     JiraBtn.classList.remove('active-jira-btn');
+    header.classList.remove(".header-wrapper-jira-svg");
+    header.classList.remove("header-wrapper-jira-svg")
   });
 
   JiraBtn.addEventListener("click", function () {
     showPage(JiraContent, ConfContent);
-    ConfBtn.classList.remove('active-conf-btn');
+    ConfBtn.classList.add('tab-conf-passive');
     JiraBtn.classList.add('active-jira-btn');
+    header.classList.add("header-wrapper-jira-svg")
   });
 
   // Initial setup: Show page 1
   showPage(ConfContent, JiraContent);
-});
+
 
 function showPage(show, hide) {
   // Show the selected page
@@ -49,7 +36,7 @@ function showPage(show, hide) {
 
 // carousel confluence
 
-const itemWidth = 226
+const itemWidth = 225
 
 const prevConf = document.querySelector('.conf-prev-button')
 const nextConf = document.querySelector('.conf-next-button')
