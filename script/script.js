f898b6379e892588015e8749c82ac8df718990b2
lisagjh <lisagjhoorn@outlook.com>
2024-01-24 20:19:00 +0100

    dark mode fixed ??

--- script/script.js
+++ script/script.js
@@ -64,3 +64,10 @@ prevJira.addEventListener("click", function(){
 nextJira.addEventListener("click", function(){
     listJira.scrollLeft += itemWidth;
 })
+
+// dark mode btn
+
+function colorSchemeToggle() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+} 
